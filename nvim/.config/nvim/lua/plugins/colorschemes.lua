316833fc7526d925129593ed1762ad2447862d59
Karthik Yadav <mail@karthikyadav.com>
2025-02-11 22:29:14 +0545

    catppuccin colorscheme added but inactive

--- nvim/.config/nvim/lua/plugins/colorschemes.lua
+++ nvim/.config/nvim/lua/plugins/colorschemes.lua
@@ -1,9 +1,17 @@
 return {
-{
-  "folke/tokyonight.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = {},
-}
-}
 
+	---@colorscheme tokyonight
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+	},
+
+	---@colorscheme catppuccin
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+	},
+}
