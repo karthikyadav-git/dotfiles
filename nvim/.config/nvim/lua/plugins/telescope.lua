78880ca68923c318c3cecbedbee44573e323965b
Karthik Yadav <mail@karthikyadav.com>
2025-02-12 21:52:43 +0545

    add telescope-ui-select

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,12 +1,23 @@
 return {
-  'nvim-telescope/telescope.nvim',
-  tag = '0.1.8',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  keys = { "<leader>f" },
-  opts = {},
-  cmd = "Telescope",
-  config = function()
-  require("config.telescope")
-  end,
+  {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.8',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    lazy = false,
+    opts = {},
+    config = function()
+      require("config.telescope")
+    end,
+  },
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+    opts = {
+      extensions = {
+        ["ui-select"] = {
+          require("telescope.themes").get_dropdown {}
+        }
+      }
+    },
+  }
 }
 
