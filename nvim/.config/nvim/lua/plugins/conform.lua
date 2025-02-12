ae3b416cd346458691b8c2b7a1c8ab4f36a86235
Karthik Yadav <mail@karthikyadav.com>
2025-02-12 22:44:11 +0545

    add "conform.nvim" formatter

---
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,16 @@
+return {
+  'stevearc/conform.nvim',
+  lazy = false,
+  opts = {
+    formatters_by_ft = {
+      lua = { "stylua" },
+      python = { "isort", "black" },
+      rust = { "rustfmt", lsp_format = "fallback" },
+      javascript = { "prettierd", "prettier", stop_after_first = true },
+    },
+    format_on_save = {
+      timeout_ms = 500,
+      lsp_format = "fallback",
+    },
+  },
+}
