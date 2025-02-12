435a0d16320f659ea24dc7a1a1e7112c21bdf340
Karthik Yadav <mail@karthikyadav.com>
2025-02-12 15:26:49 +0545

    merge treesitter config file into treesitter plugin file and activate auto_install

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -3,7 +3,13 @@ return {
   build = ":TSUpdate",
   opts = {},
   config = function()
-    require("config.treesitter")
+    local configs = require("nvim-treesitter.configs")
+    configs.setup({
+      auto_install = true,
+      sync_install = false,
+      highlight = { enable = true },
+      indent = { enable = true }
+    })
   end
 }
 
