566795f5f6d2bf6fcc3bfd58c9e70f899220877b
Karthik Yadav <mail@karthikyadav.com>
2025-02-13 22:11:24 +0545

    fix linters

--- nvim/.config/nvim/lua/plugins/nvim-lint.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lint.lua
@@ -3,13 +3,20 @@ return {
 	lazy = false,
 	config = function()
 		require("lint").linters_by_ft = {
-			javascript = { "ESLint" },
-			python = { "Flake8" },
-			c = { "Clang-Tidy" },
-			cpp = { "Clang-Tidy" },
-			sh = { "ShellCheck" },
-			bash = { "ShellCheck" },
-			zsh = { "ShellCheck" },
+			javascript = { "eslint_d" },
+			python = { "flake8" },
+			c = { "cpplint" },
+			cpp = { "cpplint" },
+			sh = { "shellcheck" },
+			bash = { "shellcheck" },
+			zsh = { "shellcheck" },
+			lua = { "luacheck" },
 		}
+
+		vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost", "InsertLeave" }, {
+			callback = function()
+				require("lint").try_lint()
+			end,
+		})
 	end,
 }
