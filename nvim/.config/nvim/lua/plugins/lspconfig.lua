207b7d3f13f6731686049435a7fab8fe1823fd0b
Karthik Yadav <mail@karthikyadav.com>
2025-02-13 22:13:37 +0545

    add blink completion and refine lspconfig

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -1,27 +1,21 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		lazy = false,
-		opts = {},
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
-		opts = {
-			ensure_installed = { "lua_ls", "ts_ls", "pyright", "clangd", "bashls" },
-			automatic_installation = true,
+	"neovim/nvim-lspconfig",
+	dependencies = { "saghen/blink.cmp" },
+
+	opts = {
+		servers = {
+			lua_ls = {},
+			ts_ls = {},
+			pyright = {},
+			clangd = {},
 		},
 	},
-	{
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		config = function()
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({})
-			lspconfig.ts_ls.setup({})
-			lspconfig.pyright.setup({})
-			lspconfig.clangd.setup({})
-			lspconfig.bashls.setup({})
-		end,
-	},
+
+	config = function(_, opts)
+		local lspconfig = require("lspconfig")
+		for server, config in pairs(opts.servers) do
+			config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
+			lspconfig[server].setup(config)
+		end
+	end,
 }
