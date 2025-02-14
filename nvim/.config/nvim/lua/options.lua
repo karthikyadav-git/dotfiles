549633f1e0497baee940c89ef02c2c4b36a64f44
Karthik Yadav <mail@karthikyadav.com>
2025-02-14 13:48:46 +0545

    add Vimtex for latex

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -1,6 +1,18 @@
 -- Indentation options
-local set = vim.cmd.set
-set("expandtab")
-set("tabstop=2")
-set("softtabstop=2")
-set("shiftwidth=2")
+vim.cmd.set("expandtab")
+vim.cmd.set("tabstop=2")
+vim.cmd.set("softtabstop=2")
+vim.cmd.set("shiftwidth=2")
+
+-- LaTeX options
+vim.g.vimtex_view_method = "zathura"
+vim.g.vimtex_compiler_method = "latexmk"
+vim.cmd.set("conceallevel=2")
+vim.g.vimtex_syntax_conceal = {
+	math_bounds = 1,
+	math_delimiters = 1,
+	math_fracs = 1, -- Conceals \frac
+	math_super_sub = 1,
+	math_symbols = 1,
+	sections = 1,
+}
