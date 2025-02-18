4277d3cf53970fa538746d9b94169af146a0970e
Karthik Yadav <mail@karthikyadav.com>
2025-02-18 22:20:51 +0545

    mason and lspconfig merged into one file

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -1,25 +1,38 @@
 return {
-	"neovim/nvim-lspconfig",
-	dependencies = { "saghen/blink.cmp" },
-
-	opts = {
-		servers = {
-			javascript = { "eslint_d" },
-			python = { "flake8" },
-			c = { "cpplint" },
-			cpp = { "cpplint" },
-			sh = { "shellcheck" },
-			bash = { "shellcheck" },
-			zsh = { "shellcheck" },
-			lua = { "luacheck" },
+	{
+		"williamboman/mason.nvim",
+		opts = {},
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		opts = {
+			ensure_installed = {},
+			automatic_installation = true,
 		},
 	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = { "saghen/blink.cmp" },
 
-	config = function(_, opts)
-		local lspconfig = require("lspconfig")
-		for server, config in pairs(opts.servers) do
-			config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
-			lspconfig[server].setup(config)
-		end
-	end,
+		opts = {
+			servers = {
+				javascript = { "eslint_d" },
+				python = { "flake8" },
+				c = { "cpplint" },
+				cpp = { "cpplint" },
+				sh = { "shellcheck" },
+				bash = { "shellcheck" },
+				zsh = { "shellcheck" },
+				lua = { "luacheck" },
+			},
+		},
+
+		config = function(_, opts)
+			local lspconfig = require("lspconfig")
+			for server, config in pairs(opts.servers) do
+				config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
+				lspconfig[server].setup(config)
+			end
+		end,
+	},
 }
