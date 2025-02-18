b03a7a9c8ffd7efe7c592e536d3fbb4b3727834e
Karthik Yadav <mail@karthikyadav.com>
2025-02-18 22:25:30 +0545

    catppuccin colorscheme config upgrade

--- nvim/.config/nvim/lua/plugins/colorschemes.lua
+++ nvim/.config/nvim/lua/plugins/colorschemes.lua
@@ -2,8 +2,8 @@ return {
 
 	---@colorscheme tokyonight
 	{
-    "folke/tokyonight.nvim",
-    priority = 1000,
+		"folke/tokyonight.nvim",
+		priority = 1000,
 		opts = {},
 	},
 
@@ -12,6 +12,26 @@ return {
 		"catppuccin/nvim",
 		name = "catppuccin",
 		priority = 1000,
-    opts = {},
+		opts = {
+			flavour = "mocha", -- Set Mocha variant
+			integrations = {
+				treesitter = true,
+				native_lsp = {
+					enabled = true,
+					virtual_text = {
+						errors = { "italic" },
+						hints = { "italic" },
+						warnings = { "italic" },
+						information = { "italic" },
+					},
+					underlines = {
+						errors = { "underline" },
+						hints = { "underline" },
+						warnings = { "underline" },
+						information = { "underline" },
+					},
+				},
+			},
+		},
 	},
 }
