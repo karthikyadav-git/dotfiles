634c4b5afc075d90eab815571558ab68742a1131
Karthik Yadav <mail@karthikyadav.com>
2025-02-18 23:35:22 +0545

    add nvim-ts-autotag plugin

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,10 +1,16 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	opts = {
-		auto_install = true,
-		sync_install = false,
-		highlight = { enable = true },
-		indent = { enable = true },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		opts = {
+			auto_install = true,
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
+		},
+	},
+	{
+		"windwp/nvim-ts-autotag",
+		opts = {},
 	},
 }
