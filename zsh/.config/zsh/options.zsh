b57e4cc6384cdda904f9ee01655984070b046d4c
Karthik Yadav <mail@karthikyadav.com>
2025-02-22 21:32:36 +0545

    add options

--- zsh/.config/zsh/options.zsh
+++ zsh/.config/zsh/options.zsh
@@ -11,9 +11,18 @@ setopt EXTENDED_GLOB
 
 # Share history across sessions
 setopt SHARE_HISTORY
+
 setopt APPEND_HISTORY
 setopt INC_APPEND_HISTORY
+
+# Prevent duplicate history
 setopt HIST_IGNORE_DUPS
+setopt HIST_IGNORE_ALL_DUPS
+setopt HIST_SAVE_NO_DUPS
+setopt HIST_FIND_NO_DUPS
+
+# Do not write commands starting with a space to history
+setopt HIST_IGNORE_SPACE
 
 # Automatically list completion options
 setopt AUTO_LIST
