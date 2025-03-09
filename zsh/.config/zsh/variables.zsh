e31f9ac16aa27bda2ad786e37bbca11e42df811a
Karthik Yadav <mail@karthikyadav.com>
2025-03-09 13:34:17 +0545

    add fcitx5 as default input method if installed

--- zsh/.config/zsh/variables.zsh
+++ zsh/.config/zsh/variables.zsh
@@ -2,3 +2,11 @@
 export SSH_AUTH_SOCK="$(echo /tmp/ssh-*/agent.*)"
 export SSH_AGENT_PID="$(expr $(ls /tmp/ssh-*/ | tr -d "agent.") + 1)"
 
+# Fcitx5 (a language input framework) configuration variables
+if [[ -x "$(command -v fcitx5)" ]]; then
+  export GTK_IM_MODULE=fcitx
+  export QT_IM_MODULE=fcitx
+  export XMODIFIERS=@im=fcitx
+  export INPUT_METHOD=fcitx
+fi
+
