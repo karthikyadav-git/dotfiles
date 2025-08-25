622ae0373beae2add89eed82d32456afca4c34a3
Karthik Yadav <mail@karthikyadav.com>
2025-08-25 19:24:11 +0545

    Switched from Neo-tree to Snack Explorer

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -1,7 +1,7 @@
 ---@plugin neo-tree
-vim.keymap.set('n', '<leader>e', ':Neotree filesystem reveal left<CR>', {})
+-- vim.keymap.set('n', '<leader>e', ':Neotree filesystem reveal left<CR>', {})
 
 -- lsp
-vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
