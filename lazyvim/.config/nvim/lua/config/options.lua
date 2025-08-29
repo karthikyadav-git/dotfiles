9fa73e490d2f3e1fb585b3f79d3c83f9e984509b
Karthik Yadav <mail@karthikyadav.com>
2025-08-29 16:36:28 +0545

    build directory added to lazyvim

--- lazyvim/.config/nvim/lua/config/options.lua
+++ lazyvim/.config/nvim/lua/config/options.lua
@@ -1,3 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- VimTex options
+vim.g.vimtex_view_method = "zathura"
+vim.g.vimtex_compiler_latexmk = {
+  options = {
+    "-outdir=build",
+  },
+}
