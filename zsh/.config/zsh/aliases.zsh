359ce9f4360fa32c9a31aa8ae236968e40de15f0
Karthik Yadav <mail@karthikyadav.com>
2025-09-05 02:23:53 +0545

    config for bat added

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -1,2 +1,15 @@
-# Set aliases here
+# Alias for lsd
+if [[ -x "$(command -v lsd)" ]]; then
+  alias ls='lsd'
+  alias l='ls -l'
+  alias la='ls -a'
+  alias lla='ls -la'
+  alias lt='ls --tree'
+fi
 
+# Alias for bat
+if [[ -x "$(command -v bat)" ]]; then
+  # help results in bat
+  alias -g -- -h='-h 2>&1 | bat --language=help --style=plain'
+  alias -g -- --help='--help 2>&1 | bat --language=help --style=plain'
+fi
