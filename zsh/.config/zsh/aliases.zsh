6e31edec5a69b460b6db9493ae54634242d6b0a3
Karthik Yadav <mail@karthikyadav.com>
2025-09-06 13:16:57 +0545

    fzf-tab setup

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -9,7 +9,9 @@ fi
 
 # Alias for bat
 if [[ -x "$(command -v bat)" ]]; then
+
   # help results in bat
   alias -g -- -h='-h 2>&1 | bat --language=help --style=plain'
   alias -g -- --help='--help 2>&1 | bat --language=help --style=plain'
+
 fi
--- zsh/.config/zsh/variables.zsh
+++ zsh/.config/zsh/variables.zsh
@@ -1,3 +1,7 @@
+# default editor
+export EDITOR=nvim
+export SUDO_EDITOR=nvim
+
 # SSH agent persists every terminal session
 export SSH_AUTH_SOCK="$(echo /tmp/ssh-*/agent.*)"
 export SSH_AGENT_PID="$(expr $(ls /tmp/ssh-*/ | tr -d "agent.") + 1)"
@@ -10,3 +14,7 @@ if [[ -x "$(command -v fcitx5)" ]]; then
   export INPUT_METHOD=fcitx
 fi
 
+# fzf environment variables
+if [[ -x "$(command -v fzf)" ]]; then
+  export FZF_DEFAULT_OPTS="--tmux='center,70%' --style=full --prompt='-> ' --marker='>>' --preview-label=' Preview '"
+fi
